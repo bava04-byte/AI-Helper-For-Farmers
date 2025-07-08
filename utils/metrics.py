e8b6a6d5670d132b86d0ba64bda2f830ecd24e46
bava04-byte <bavasinan03@gmail.com>
2025-07-08 12:48:30 +0530

    Add files via upload

--- utils/metrics.py
+++ utils/metrics.py
@@ -3,5 +3,6 @@ from torchvision import models, transforms
 from torch.utils.data import DataLoader
 from torchvision.datasets import ImageFolder
 import os
+
 def get_pytorch_model_accuracy(*args, **kwargs):
     return 95.5  # Use a fixed placeholder accuracy
